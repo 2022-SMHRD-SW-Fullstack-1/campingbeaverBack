16d3c9655f131aabcb3cd62dbb6f071d673074d9
임아해 <limahae1015@gmail.com>
2022-10-26 16:05:10 +0900

    기본설정 다시 / 아해

--- CampingBeaver/src/main/java/com/example/demo/controller/HelloController.java
+++ CampingBeaver/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,10 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-	@GetMapping("/boot")
+	@GetMapping("/main")
 	public String root() {
 		return "main";
 	}
