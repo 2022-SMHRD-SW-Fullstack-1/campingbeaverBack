01982c69b8b1dc94fedfa9c35e6a27c038e1cd69
임아해 <limahae1015@gmail.com>
2022-10-28 14:39:36 +0900

    머지전 푸시 /아해

--- CampingBeaver/src/main/java/com/example/demo/controller/HelloController.java
+++ CampingBeaver/src/main/java/com/example/demo/controller/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@GetMapping("/main")
+	@GetMapping("/hello")
 	public String root() {
-		return "main";
+		return "스프링부트에서 쓴거임";
 	}
 }
