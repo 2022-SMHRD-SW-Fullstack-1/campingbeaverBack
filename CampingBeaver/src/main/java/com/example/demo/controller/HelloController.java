5eaf96bdec3c195c483fe7a427577ad39c6552a4
yeonjuko <yjoogo@gmail.com>
2022-11-01 16:56:11 +0900

    지도 위치 받기/연주

--- CampingBeaver/src/main/java/com/example/demo/controller/HelloController.java
+++ CampingBeaver/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.campsite_info;
+import com.example.demo.service.RecomService;
 
 //@Controller
 //public class HelloController {
@@ -27,15 +32,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@GetMapping("/main")
-	public String root() {
-		return "main";
-	}
-	@PostMapping("/main")
-	public com.example.demo.model.reservation_info reservation_info() {
-		com.example.demo.model.reservation_info reservation_info = new com.example.demo.model.reservation_info(1, "주상민", "2022-11-05", "2022-11-06");
-		return reservation_info;
-	}
+	
+	@Autowired
+	RecomService recomService;
+	
+//	@GetMapping("/main")
+//	public String root() {
+//		return "main";
+//	}
+//	@PostMapping("/main")
+//	public com.example.demo.model.reservation_info reservation_info() {
+//		com.example.demo.model.reservation_info reservation_info = new com.example.demo.model.reservation_info(1, "주상민", "2022-11-05", "2022-11-06");
+//		return reservation_info;
+//	}
 //	@GetMapping("/beaver/main")
 //	public void reservation(@RequestParam Integer reserv_seq, @RequestParam String user_id, @RequestParam Integer pkg_seq, @RequestParam String reserv_name, @RequestParam String reserv_addr, @RequestParam String reserv_post, @RequestParam String reserv_phone, @RequestParam Integer reserv_price, @RequestParam String reserv_pay, @RequestParam String reserv_s_date, @RequestParam String reserv_e_date ) {
 //        System.out.println(reserv_seq);
@@ -50,4 +59,22 @@ public class HelloController {
 //        System.out.println(reserv_s_date);
 //        System.out.println(reserv_e_date);
 //	}
+	
+	@GetMapping("/main")
+	public campsite_info campsite_info() {
+//		recomService.selectOneRecom(site_seq);
+//		campsite_info campsite_info = new campsite_info(site_seq);
+		return 	recomService.selectOneRecom();
+	}
+	
+	@PostMapping("/hash")
+	public List<Object> hashTag(@RequestBody List<Object> hashList) {
+		System.out.println(hashList);
+		return hashList;
+	}
+	
+	@GetMapping("/hash")
+	public List<campsite_info> searchHashList() {
+		return recomService.searchHashTag();
+	}
 }
---
+++ CampingBeaver/src/main/java/com/example/demo/mapper/RecomMapper.java
@@ -0,0 +1,19 @@
+package com.example.demo.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.campsite_info;
+
+@Mapper
+public interface RecomMapper {
+	
+	//캠핑장 추천 등록 번호로 정보 불러오는 메서드
+	public campsite_info selectOneRecom();
+	
+	//해시태그로 캠핑장 검색
+	public List<campsite_info> searchHashTag();
+	
+}
