407e229303afd1cb19b46f112ff84a6b82ea98fc
주상민 <112377313+js4183@users.noreply.github.com>
2022-11-07 15:48:05 +0900

    풀/상민

--- CampingBeaver/src/main/java/com/example/demo/controller/ReservationinfoController.java
+++ CampingBeaver/src/main/java/com/example/demo/controller/ReservationinfoController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.basketlist;
+import com.example.demo.model.reservation_info;
+import com.example.demo.model.reservationlist;
 import com.example.demo.service.ReservationinfoService;
 import com.google.gson.Gson;
 
@@ -22,5 +27,9 @@ public class ReservationinfoController {
 		System.out.println(jsonStr);
 		reservationinfoService.reserv(map);
 	}
-
+	
+	@GetMapping("/ordercom")
+	public List<reservationlist> selectOrderCom(){
+		return reservationinfoService.selectOrderCom();
+	}
 }
